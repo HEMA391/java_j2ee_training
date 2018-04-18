b16017ebce165678a3c75b374e34c852f9c3d34b
isquarebsys <isquarebsys@gmail.com>
2018-04-18 21:30:48 +0530

    Java 8 features

---
+++ Core/src/java8/MyConsumer.java
@@ -0,0 +1,8 @@
+package java8;
+import java.util.function.Consumer;
+
+class MyConsumer implements Consumer<Integer>{
+	public void accept(Integer t) {
+		System.out.println("Consumer impl Value::"+t);
+	}
+}
