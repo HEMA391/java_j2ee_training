f33cc428b123e5f17213e43a02d99bccb9f3339d
isquarebsys <isquarebsys@gmail.com>
2018-04-19 13:59:32 +0530

    Java 8

--- Core/src/functions/ConsumerExample1.java
+++ Core/src/functions/ConsumerExample1.java
@@ -1,19 +1,41 @@
 package functions;
 
-import java.util.function.Consumer;  
+import java.util.ArrayList;
+import java.util.List;
+import java.util.function.Consumer;
+
 public class ConsumerExample1 {
-	static void printMessage(String name){  
-        System.out.println("Hello "+name);  
-    }  
-    static void printValue(int val){  
-        System.out.println(val);  
-    }  
-    public static void main(String[] args) {  
-        // Referring method to String type Consumer interface   
-        Consumer<String> consumer1 = ConsumerExample1::printMessage;  
-        consumer1.accept("John");   // Calling Consumer method  
-        // Referring method to Integer type Consumer interface  
-        Consumer<Integer> consumer2 = ConsumerExample1::printValue;  
-        consumer2.accept(12);   // Calling Consumer method  
-    }  
+	static void printMessage(String name) {
+		System.out.println("Hello " + name);
+	}
+
+	static void printValue(int val) {
+		System.out.println(val);
+	}
+
+	static void addList(List<Integer> list) {
+		// Return sum of list values
+		int result = list.stream().mapToInt(Integer::intValue).sum();
+		System.out.println("Sum of list values: " + result);
+	}
+
+	public static void main(String[] args) {
+		// Referring method to String type Consumer interface
+		Consumer<String> consumer1 = ConsumerExample1::printMessage;
+		consumer1.accept("John"); // Calling Consumer method
+		// Referring method to Integer type Consumer interface
+		Consumer<Integer> consumer2 = ConsumerExample1::printValue;
+		consumer2.accept(12); // Calling Consumer method
+
+		// Example 2
+		// Creating a list and adding values
+		List<Integer> list = new ArrayList<Integer>();
+		list.add(10);
+		list.add(20);
+		list.add(30);
+		list.add(40);
+		// Referring method to String type Consumer interface
+		Consumer<List<Integer>> consumer = ConsumerExample1::addList;
+		consumer.accept(list); // Calling Consumer method
+	}
 }
